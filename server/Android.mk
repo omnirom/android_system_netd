omapzoom/platform-system-netd

netd: Don't reload fw if a single fw is supported
TI's mac80211 solution uses only a single fw,
so return immediately in case of fwreload command.

Change-Id: I4157d50a3c29a0053caf099f51bb78077d16af17
Signed-off-by: Eliad Peller <eliad@wizery.com>

Android.mk
@@ -38,6 +38,10 @@ LOCAL_C_INCLUDES := $(KERNEL_HEADERS) \
 
 LOCAL_CFLAGS := -Werror=format
 
+ifdef USES_TI_MAC80211
+LOCAL_CFLAGS += -DSINGLE_WIFI_FW
+endif
+
 LOCAL_SHARED_LIBRARIES := libstlport libsysutils liblog libcutils libnetutils \
                           libcrypto libhardware_legacy libmdnssd libdl \
                           liblogwrap

omnirom/android_system_netd

server/Android.mk
@@ -26,6 +26,10 @@ LOCAL_CLANG := true
 LOCAL_CPPFLAGS := -std=c++11 -Wall -Werror
 LOCAL_MODULE := netd
 
+ifdef USES_TI_MAC80211
+LOCAL_CPPFLAGS += -DSINGLE_WIFI_FW
+endif
+
 LOCAL_SHARED_LIBRARIES := \
         libcrypto \
         libcutils \